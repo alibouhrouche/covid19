9edd4922773cf96dd4d925b8728be7a91613b9b2
Glitch (sarscov2) <none>
2020-03-20 17:02:50 +0000

    ⛰🍬 Checkpoint ./public/sw.js:3802994/5515

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+var version = 'v1::1::';
+
+self.addEventListener("install", function(event) {
+    console.log('WORKER: install event in progress.');
+    event.waitUntil(
+      caches
+        .open(version + 'assets')
+        .then(function(cache) {
+          return cache.addAll([
+            '/',
+            '/style.css',
+            '/script.js',
+            '/icons.svg',
+            '/freakflags.css',
+            '/script.js'
+          ]);
+        })
+        .then(function() {
+          console.log('WORKER: install completed');
+        })
+    );
+  });
