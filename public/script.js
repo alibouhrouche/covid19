6582d25e034a4d8e913e121177e0e47d82ae6b95
Glitch (sarscov2) <none>
2020-03-20 17:26:49 +0000

    🚋👦 Checkpoint ./public/script.js:3802994/798

--- public/script.js
+++ public/script.js
@@ -1,4 +1,15 @@
 "use strict";
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('/sw.js').then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
 function isOnline () {
   var connectionStatus = document.getElementById('nonet');
 
@@ -8,7 +19,6 @@ function isOnline () {
     connectionStatus.style.display = "block";
   }
 }
-
 window.addEventListener('online', isOnline);
 window.addEventListener('offline', isOnline);
 isOnline();
