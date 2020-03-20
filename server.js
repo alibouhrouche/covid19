8a46e8626ad0766ee30aa80c06aae8cf57f1bea7
Glitch (sarscov2) <none>
2020-03-20 18:40:09 +0000

    🐋🐛 Checkpoint ./server.js:3802994/2803 ./public/sw.js:3802994/198 ./public/index.html:3802994/125 ./package.json:3802994/1427

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var express = require("express");
 var async  = require('express-async-await');
 var fetch = require('node-fetch');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 var countries = require('./list.js');
 var app = express();
+
+const options = {
+  target: 'https://cdn.glitch.com/', // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: false, // proxy websockets
+  pathRewrite: {
+    '^/favicon.ico': '/f2f5091a-5f0a-4796-94fa-c7393a3b1aae/favicon.ico?v=1584540676955', // rewrite path
+    '^/api/remove/path': '/path' // remove base path
+  },
+  router: {
+    // when request.headers.host == 'dev.localhost:3000',
+    // override target 'http://www.example.org' to 'http://localhost:8000'
+    'dev.localhost:3000': 'http://localhost:8000'
+  }
+};
 
 function checkHttps(req, res, next) {
   // protocol check, if http, redirect to https
@@ -25,22 +41,22 @@ app.get("/favicon.ico",async (req, res, next) => {
   const processData = async () => {
   const Data = await foundData()
   var ResponseData = await Data.buffer()
-
+  res.set('Content-Type', 'image/x-icon')
   res.send(ResponseData);
   }
   processData()
 });
 app.get("/flags",async (req, res, next) => {
-     function foundData(){
+  function foundData(){
        return fetch("https://cdn.glitch.com/f2f5091a-5f0a-4796-94fa-c7393a3b1aae/flagSprite60.png?v=1584651917190")
   }
   const processData = async () => {
   const Data = await foundData()
-  var ResponseData = await Data.buffer()
+  var ResponseData = await Data.buffer() 
+  res.set('Content-Type', 'image/png')
   res.send(ResponseData);
   }
   processData()
-  res.set('Content-Type', 'image/png')
 });
 app.get("/data",async (request, response, next) => {
   function foundData(){
